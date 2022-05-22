50dfd393ca9d2d9b551df19cfd814bdae964182d
sailikpandey22 <102276321+sailikpandey22@users.noreply.github.com>
2022-05-22 22:52:11 +0530

    fixed bug v1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomNumber1 = Math.random();
-randomNumber1 = randomNumber1 * 6 + 1;
+randomNumber1 = randomNumber1 * 6;
 randomNumber1 = Math.floor(randomNumber1);
 
 var randomNumber2 = Math.random();
-randomNumber2 = randomNumber2 * 6 + 1;
+randomNumber2 = randomNumber2 * 6;
 randomNumber2 = Math.floor(randomNumber2);
 // console.log(randomNumber1);
 
