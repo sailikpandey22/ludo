475f6f7bbfc1e62c19c07e44076dbec39f693d1b
sailikpandey22 <102276321+sailikpandey22@users.noreply.github.com>
2022-05-22 22:45:20 +0530

    added intitial files

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1 * 6 + 1;
+randomNumber1 = Math.floor(randomNumber1);
+
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber2 * 6 + 1;
+randomNumber2 = Math.floor(randomNumber2);
+// console.log(randomNumber1);
+
+var images = [
+  "images/dice1.png",
+  "images/dice2.png",
+  "images/dice3.png",
+  "images/dice4.png",
+  "images/dice5.png",
+  "images/dice6.png",
+];
+// document
+//   .querySelector(".img1")
+//   .setAttribute("src", "images/dice[randomNumber1].png");
+
+document.getElementById("img1").src = images[randomNumber1];
+
+document.getElementById("img2").src = images[randomNumber2];
